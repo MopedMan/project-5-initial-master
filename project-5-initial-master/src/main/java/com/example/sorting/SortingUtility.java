dfc593c83dfbf6d7ac59ebb9456aa56dedbc69df
BuildTools <unconfigured@null.spigotmc.org>
2022-04-29 01:22:22 -0700

    Initial commit

---
+++ project-5-initial-master/src/main/java/com/example/sorting/SortingUtility.java
@@ -0,0 +1,104 @@
+package com.example.sorting;
+
+public class SortingUtility {
+
+    /**
+     * Part A Gnome Sort Algorithm
+     * <p>
+     * Implement gnome sort per this pseudocode.
+     * <p>
+     * <pre>
+     *    method gnomeSort(a[])
+     *       pos = 0
+     *       while pos < length(a)
+     *          if (pos == 0 or a[pos] >= a[pos-1])
+     *          pos = pos + 1
+     *       else
+     *          swap a[pos] and a[pos-1]
+     *          pos = pos - 1
+     * </pre>
+     *
+     * @param data
+     * @param <T>
+     * @see <a href="https://en.wikipedia.org/wiki/Gnome_sort">https://en.wikipedia.org/wiki/Gnome_sort</a>
+     */
+    public static <T extends Comparable<T>> void gnomeSort(T[] data) {
+
+        int position = 0;
+        while (position < data.length) {
+            if (position == 0) {
+                position++;
+            }
+            if (data[position].compareTo(data[position - 1]) > 0) {
+                position++;
+            } else {
+                swap(data, position, position - 1);
+                position--;
+            }
+        }
+
+    }
+
+    /**
+     * Part B Optimized Gnome Sort Algorithm.
+     * <p>
+     * Implement an optimized gnome sort per the pseudocode below.
+     * <p>
+     * <pre>
+     *    method gnomierSort(a[])
+     *       for pos in 1 to length(a)
+     *       gnomierSort(a, pos)
+     * </pre>
+     *
+     * @param data
+     * @param <T>
+     * @see <a href="https://en.wikipedia.org/wiki/Gnome_sort">https://en.wikipedia.org/wiki/Gnome_sort</a>
+     */
+    public static <T extends Comparable<T>> void gnomierSort(T[] data) {
+
+        for (int position = 1; position < data.length; position++) {
+            gnomierSort(data, position);
+        }
+
+    }
+
+    /**
+     * Part C Optimized Gnome Sort Algorithm
+     * <p>
+     * Implement an optimized gnome sort per the pseudocode below.
+     * <p>
+     * <pre>
+     *    method gnomierSort(a[], upperBound)
+     *       pos = upperBound
+     *       while pos > 0 and a[pos-1] > a[pos]
+     *          swap a[pos-1] and a[pos]
+     *          pos = pos - 1
+     * </pre>
+     *
+     * @param data
+     * @param <T>
+     */
+    private static <T extends Comparable<T>> void gnomierSort(T[] data, int upperBound) {
+
+        int position = upperBound;
+
+        while (position > 0 && data[position - 1].compareTo(data[position]) > 0) {
+            swap(data, position - 1, position);
+            position--;
+        }
+
+    }
+
+    private static <T extends Comparable<T>> void swap(T[] data, int index1, int index2) {
+
+        T temp = data[index1];
+        data[index1] = data[index2];
+        data[index2] = temp;
+
+    }
+}
+
+
+
+
+
